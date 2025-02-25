3cb2e8b99a45e8c88566f52d5b7ee293544f7d64
ling-0423 <2102161238@qq.com>
2025-02-25 15:47:04 +0800

    前期准备

---
+++ README.md
@@ -0,0 +1,9 @@
+使用vue3,含pc端和移动端
+
+
+
+git add *
+
+git commit -m "注释"
+
+git push -u origin main
---
+++ pzadmin/src/router/index.js
@@ -0,0 +1,14 @@
+import Layout from '../views/Main.vue'
+import Login from '../views/login/index.vue'
+
+const routes = [
+    {
+        path:'/',
+        component:Layout
+    },
+
+    {
+        path:'/login',
+        component:Login
+    }
+]
